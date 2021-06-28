b4242368527eb8e1998aeba4fd8e44df60f190d8
ERFANIUM <officialerfanshield@gmail.com>
2021-06-29 01:19:49 +0430

    delete serializing lifecycle

--- lib/lifecycles.ts
+++ lib/lifecycles.ts
@@ -90,16 +90,16 @@ async function handling(app: Espresso, request: ESRequest, reply: ESReply) {
   reply.send(body);
 }
 
-// LC
-async function serializing(app: Espresso, request: ESRequest, reply: ESReply) {
-  const serializedBody = app.serializer(request, reply);
-  await reply.responseWith(
-    new Response(serializedBody, {
-      headers: reply.headers,
-      status: reply.statusCode,
-    }),
-  );
-}
+// // LC
+// function serializing(app: Espresso, request: ESRequest, reply: ESReply): Body | BodyInit {
+//   return app.serializer(request, reply);
+//   // await reply.responseWith(
+//   //   new Response(serializedBody, {
+//   //     headers: reply.headers,
+//   //     status: reply.statusCode,
+//   //   }),
+//   // );
+// }
 
 //LC
 async function errorHandling(
@@ -134,8 +134,12 @@ export async function start(app: Espresso, request: ESRequest, reply: ESReply) {
     );
   } catch (error) {
     await errorHandling(app, request, reply, error);
-  } finally {
-    await serializing(app, request, reply);
-    reply.responseTime = performance.now() - reply.createdAt;
   }
+
+  const serializedBody = app.serializer(request, reply);
+  reply.responseTime = performance.now() - reply.createdAt;
+  return new Response(serializedBody, {
+    headers: reply.headers,
+    status: reply.statusCode,
+  });
 }
