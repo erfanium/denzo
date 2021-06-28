2be65781e18210f5c97c116e1d6782c0380262d6
ERFANIUM <officialerfanshield@gmail.com>
2021-06-28 13:56:07 +0430

    change router import url

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
 export { default as Ajv } from "https://esm.sh/ajv@8.6.0";
-export { Node } from "https://raw.githubusercontent.com/erfanium/router/generic/mod.ts";
+export { Node } from "https://deno.land/x/router@v2.0.1/mod.ts";
