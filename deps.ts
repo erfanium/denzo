279ce57962332a85432ecaa3f0159a5a467be0e4
Erfan Safari <erfanshield@outlook.com>
2021-09-12 14:22:59 +0430

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
-export { serve } from "https://deno.land/x/native_http@0.3.1/mod.ts";
+export { serve } from "https://deno.land/x/native_http@1.0.0/mod.ts";
 export type {
   HTTPOptions,
   HTTPSOptions,
-} from "https://deno.land/x/native_http@0.4.0/mod.ts";
+} from "https://deno.land/x/native_http@1.0.0/mod.ts";
 export { Node } from "https://deno.land/x/router@v2.0.1/mod.ts";
-export { default as Ajv } from "https://esm.sh/ajv@8.6.0";
+export { default as Ajv } from "https://esm.sh/ajv@8.6.2";
